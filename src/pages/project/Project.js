a52233b5c34fd5e2bad84fbf10072b32f77983b4
Im-Hassan-wd <alhassansahih@gmail.com>
2023-04-18 14:23:01 +0100

    about to merge

--- src/pages/project/Project.js
+++ src/pages/project/Project.js
@@ -1,28 +1,27 @@
-import { useParams } from 'react-router-dom'
-import { useDocument } from '../../hooks/useDocument'
+import { useParams } from "react-router-dom";
+import { useDocument } from "../../hooks/useDocument";
 
 // styles
-import './Project.css'
-import ProjectComment from './ProjectComment'
-import ProjectSummary from './ProjectSummary'
-
+import "./Project.css";
+import ProjectComment from "./projectComment";
+import ProjectSummary from "./ProjectSummary";
 
 export default function Project() {
-  const { id } = useParams()
-  const { error, document } = useDocument('projects', id)
+  const { id } = useParams();
+  const { error, document } = useDocument("projects", id);
 
-  if(error) {
-    return <div className='error'>{error}</div>
+  if (error) {
+    return <div className="error">{error}</div>;
   }
 
-  if(!document) {
-    return <div className='loading'>loading...</div>
+  if (!document) {
+    return <div className="loading">loading...</div>;
   }
 
   return (
-    <div className='project-details'>
+    <div className="project-details">
       <ProjectSummary project={document} />
       <ProjectComment project={document} />
     </div>
-  )
+  );
 }
